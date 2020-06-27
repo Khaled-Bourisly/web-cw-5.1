308604a3787a6c3967cdfdf6cfaa61d654f8ad29
Khaled-Bourisly <bourislyk00@gmail.com>
2020-06-27 20:38:50 +0300

    javascript1

---
+++ scripts.js
@@ -0,0 +1,34 @@
+const name="خالد"
+
+const age= 16
+
+console.log ("اسمي خالد وانا عمر ي16")
+
+console.log("name,age")
+
+console.log(age*5)
+
+console.log(5)
+
+const myhobbies=["table-tennis","football","volleyball"]
+
+myhobbies.push(16)
+
+console.log(myhobbies)
+
+myhobbies.pop()
+
+let web = {language:"css"}
+
+let assistants = ["Hussain" , "nancy","Rahaf","Moudhi"]
+
+let tracks = {
+    language : "css",
+    mytrack : "web-development",
+    assistants:["Hussain" , "nancy","Rahaf","Moudhi"]
+}
+
+console.log(assistants [3])
+
+
+
